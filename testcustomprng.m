cf9cb96cdf8c4ccf76521fae6eeafeb600f26ee7
shahrearkhan <77114327+shahrearkhan@users.noreply.github.com>
2021-03-01 12:08:30 -0600

    Update testcustomprng.m

--- testcustomprng.m
+++ testcustomprng.m
@@ -1,13 +1,13 @@
 %% Histograms of uniform and normal probability distribution functions
 
 % Generate uniform pdf
-u = customrand(-2,1);
+u = customrand(-2, 1, 10000);
 % Generate theoretical uniform pdf
 a = 1/(1-(-2));
 u_th = ones(1,10000)*a;
 
 % Generate normal pdf
-n = customrandn(1.5,2.0);
+n = customrandn(1.5,2.0,10000);
 % Generate theoretical normal pdf
 x = -4:0.2:8; % create bins
 n_th = normpdf(x, 2.0, 1.5);
