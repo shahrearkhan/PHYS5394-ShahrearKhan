f516e0b808a8182788cc6b50c9135762a07f8b33
shahrearkhan <77114327+shahrearkhan@users.noreply.github.com>
2021-03-01 12:09:23 -0600

    Update customrand.m

--- customrand.m
+++ customrand.m
@@ -1,5 +1,5 @@
-%% Function to generate 10000 trial values with uniform probability distributions
-function y = customrand(a,b)
-    x = rand(1,10000);
+%% Function to generate trial values with uniform probability distributions
+function y = customrand(a,b,c)
+    x = rand(1,c);
     y = x*(b-a) + a;
 end
