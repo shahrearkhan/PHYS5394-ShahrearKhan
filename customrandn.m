d6c893c524198aec2a30c5beb86c439fbabebe8e
shahrearkhan <77114327+shahrearkhan@users.noreply.github.com>
2021-03-17 10:21:06 -0500

    Revert "Revert "Update customrandn.m""
    This reverts commit 79df306e64491a74354147ea792b381b5c98bb4c.

--- customrandn.m
+++ customrandn.m
@@ -1,5 +1,5 @@
-%% Function to generate 10000 trial values with uniform probability distributions
-function y = customrandn(a,b)
-    x = randn(1,10000);
+%% Function to generate trial values with uniform probability distributions
+function y = customrandn(a,b,c)
+    x = randn(1, c);
     y = a*x + b;
 end
